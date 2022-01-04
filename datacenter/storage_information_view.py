186874e0ab3b65f9b66b221232a37894f8c08ecf
igorkorobko <nekto.korobko@gmail.com>
2022-01-04 05:00:50 +0300

    create function storage_information_view and template storage_information

---
+++ datacenter/storage_information_view.py
@@ -0,0 +1,19 @@
+from datacenter.models import Passcard
+from datacenter.models import Visit
+from django.shortcuts import render
+from django.utils.timezone import localtime
+from .models import format_duration
+
+
+def storage_information_view(request):
+    non_closed_visits = [
+        {
+            'who_entered': visit.passcard,
+            'entered_at': localtime(visit.entered_at),
+            'duration': format_duration(visit.get_duration())
+        } for visit in Visit.objects.all().filter(leaved_at=None)
+    ]
+    context = {
+        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+    }
+    return render(request, 'storage_information.html', context)
