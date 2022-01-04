7410f8b7658e87cb9b9aeb5688fd0270bcabed4a
igorkorobko <nekto.korobko@gmail.com>
2022-01-04 05:00:34 +0300

    create function passcard_info_view and template passcard_info

---
+++ datacenter/passcard_info_view.py
@@ -0,0 +1,19 @@
+from datacenter.models import Passcard
+from datacenter.models import Visit
+from django.shortcuts import render
+
+
+def passcard_info_view(request, passcode):
+    passcard = Passcard.objects.filter(passcode=passcode)
+    this_passcard_visits = [
+        {
+            'entered_at': visit.entered_at,
+            'duration': visit.get_duration(),
+            'is_strange': visit.is_visit_long()
+        }for visit in Visit.objects.filter(passcard=passcard)
+    ]
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
