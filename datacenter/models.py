db21f74636c1e49810ebac8459bd339de3246ab6
igorkorobko <nekto.korobko@gmail.com>
2022-01-04 04:59:50 +0300

    create functions get_duration, is_visit_long and format_duration

---
+++ datacenter/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils.timezone import localtime
+from math import floor
+
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return self.owner_name
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return '{user} entered at {entered} {leaved}'.format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved=(
+                'leaved at ' + str(self.leaved_at) if self.leaved_at else 'not leaved'
+            )
+        )
+
+    def get_duration(self):
+        if self.leaved_at:
+            delta = self.leaved_at - self.entered_at
+        else:
+            delta = localtime() - localtime(self.entered_at)
+        return delta
+
+    def is_visit_long(self, minutes=60):
+        return self.get_duration().total_seconds() > minutes*60
+
+
+def format_duration(duration):
+    seconds = floor(duration.total_seconds())
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    result = f'{hours}:{minutes}'
+    return result
