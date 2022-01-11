278cdef7958be0aba00f456a6420759836dd9b19
Igor <nekto.korobko@gmail.com>
2022-01-11 10:33:24 +0300

    fix allowed_host

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = [env.list('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
